61fd23b1f4c2bc900ed957593a56303eb0228f1b
ahter <ahter3@gmail.com>
2016-04-06 17:27:55 +0300

    Add static images and templates for event detail page

--- lindysite/plus/views.py
+++ lindysite/plus/views.py
@@ -4,4 +4,4 @@ from plus.models import Event
 def events_list(request):
     events = Event.objects.filter(is_published=True).order_by('start_date')
     #return render(request, 'plus/events_list.html', {'events': events})
-    return render(request, 'plus/index2.html', {'events': events})
+    return render(request, 'plus/events.html', {'events': events})
